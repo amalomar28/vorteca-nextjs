6875029c351eddc169e849f3ae15b2cfc47af9ca
amalomar28 <doseofamal@gmail.com>
2025-09-05 10:05:13 -0400

    Update login.js

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -32,6 +32,7 @@ export default function handler(req, res) {
 
   // Check password
   const isValidPassword = bcrypt.compareSync(password, user.password);
+  console.log('Password check:', { password, hash: user.password, isValid: isValidPassword })
   if (!isValidPassword) {
     return res.status(401).json({ message: 'Invalid credentials' });
   }
